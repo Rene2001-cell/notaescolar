1efd048a7cb1d0fd53afdac1987aee9bf089c69a
Rene <rene30kore@gmail.com>
2021-03-11 12:25:51 -0600

    se agregaron campos de isercion de usuarios a el administrador

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -5,5 +5,39 @@
 </head>
 <body>
 <h1>PAGINA PARA AGREGAR UN USUARIO</h1>
+{!! Form::open(['url' => 'foo/bar']) !!}
+
+    <Table>
+        <TR>
+
+            <TD>{!!Form::label('email', 'E-Mail Address')!!}</TD>
+            <TD> {!!Form::text('username')!!}</TD>
+        </TR>
+        <TR>
+
+            <TD>{!!Form::label('email', 'E-Mail Address')!!}</TD>
+            <TD> {!!Form::text('username')!!}</TD>
+        </TR>
+        <TR>
+
+            <TD>{!!Form::label('email', 'E-Mail Address')!!}</TD>
+            <TD> {!!Form::text('username')!!}</TD>
+        </TR>
+        <TR>
+
+            <TD>{!!Form::label('email', 'E-Mail Address')!!}</TD>
+            <TD> {!!Form::text('username')!!}</TD>
+        </TR>
+        <TR>
+
+            <TD>{!!Form::label('email', 'E-Mail Address')!!}</TD>
+            <TD> {!!Form::text('username')!!}</TD>
+        </TR>
+
+
+    </Table>
+
+
+{!! Form::close() !!}
 </body>
 </html>
